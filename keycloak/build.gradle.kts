c987e3fd9f50f859614bf6ef4c1acce5880d36f4
sivertheisholt <sivert.b.heisholt@protonmail.com>
2025-10-02 19:03:57 +0200

    feat: added starter setup for integration tests

--- keycloak/build.gradle.kts
+++ keycloak/build.gradle.kts
@@ -4,7 +4,8 @@ import java.util.Date
 plugins {
     base
     kotlin("jvm") version "2.2.20"
-    id("com.avast.gradle.docker-compose") version "0.17.12"
+    id("com.avast.gradle.docker-compose") version "0.17.13"
+    id("com.github.ben-manes.versions") version "0.53.0"
 }
 
 group = "no.fintlabs"
@@ -17,30 +18,14 @@ allprojects {
     }
 }
 
-val keycloakTestImageTag = "flais-keycloak:test"
-val keycloakTestImage = tasks.register<Exec>("keycloakTestImage") {
-    group = "docker"
-    description = "Build the local Keycloak image used by tests"
-    commandLine("docker", "build", "-t", keycloakTestImageTag, ".")
+dockerCompose {
+    environment.put("KEYCLOAK_VERSION", libs.versions.keycloak.get())
 }
-
-// subprojects {
-//     plugins.withId("java") {
-//         the<JavaPluginExtension>().toolchain {
-//             languageVersion.set(JavaLanguageVersion.of(21))
-//         }
-//     }
-
-//     tasks.withType<Test>().configureEach {
-//         useJUnitPlatform()
-//         dependsOn(keycloakTestImage)
-//         systemProperty("keycloak.image", keycloakTestImageTag)
-//     }
-// }
 
 tasks.register("deployDev") {
     group = "docker"
     description = "Deploy local dev with compose"
+
     doLast {
         dockerCompose.dockerExecutor.execute("compose", "up", "-d", "--build", "keycloak")
         println("Rebuilt & restarted Keycloak")
@@ -50,6 +35,7 @@ tasks.register("deployDev") {
 tasks.register("restart") {
     group = "docker"
     description = "Rebuild the keycloak image and recreate the container"
+
     doLast {
         dockerCompose.dockerExecutor.execute("compose", "build", "--pull", "keycloak")
         dockerCompose.dockerExecutor.execute("compose", "up", "-d", "--force-recreate", "keycloak")
@@ -60,9 +46,18 @@ tasks.register("restart") {
 tasks.register<Exec>("buildImage") {
     group = "docker"
     description = "Build the flais Keycloak image locally with timestamp"
+
     doFirst {
         val timestamp = SimpleDateFormat("yyyy.MM.dd-HH.mm").format(Date())
         println("Building image with tag: $timestamp")
         commandLine("docker", "build", "-t", "flais-keycloak:$timestamp", ".")
+    }
+}
+
+tasks.register<Exec>("checkDeps") {
+    group = "tools"
+    description = "Check dependencies for new versions"
+    doFirst {
+        commandLine("./gradlew", "dependencyUpdates")
     }
 }
