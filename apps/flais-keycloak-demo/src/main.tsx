d54a308a9184dac5a4f0a7361214ac3bfd1ff582
sivertheisholt <sivert.b.heisholt@protonmail.com>
2025-11-06 15:36:19 +0100

    fix(apps): make keycloak configurable from env variables

--- apps/flais-keycloak-demo/src/main.tsx
+++ apps/flais-keycloak-demo/src/main.tsx
@@ -1,24 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import Keycloak from 'keycloak-js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Keycloak from "keycloak-js";
 
 // Initialize Keycloak
 const keycloak = new Keycloak({
-  url: import.meta.env.VITE_KEYCLOAK_URL ?? 'http://localhost:8890',
-  realm: 'external',
-  clientId: 'flais-keycloak-demo',
-})
+    url: import.meta.env.VITE_KEYCLOAK_URL ?? "http://localhost:8890",
+    realm: import.meta.env.VITE_REALM ?? "external",
+    clientId: import.meta.env.VITE_CLIENT_ID ?? "flais-keycloak-demo",
+});
 
 keycloak.init().then((authenticated) => {
-  if (authenticated) {
-    createRoot(document.getElementById('root')!).render(
-      <StrictMode>
-        <App keycloak={keycloak} />
-      </StrictMode>
-    )
-  } else {
-    keycloak.login()
-  }
-})
+    if (authenticated) {
+        createRoot(document.getElementById("root")!).render(
+            <StrictMode>
+                <App keycloak={keycloak} />
+            </StrictMode>
+        );
+    } else {
+        keycloak.login();
+    }
+});
