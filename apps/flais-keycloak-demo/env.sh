5c044ce411d0ce2270d8bb562fac2a88bf671177
sivertheisholt <sivert.b.heisholt@protonmail.com>
2025-11-07 08:06:13 +0100

    feat(apps): runtime variables for flais-keycloak-demo

---
+++ apps/flais-keycloak-demo/env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+for i in $(env | grep MY_APP_) // PREFIXED FOR MY_APP_
+do
+    key=$(echo $i | cut -d '=' -f 1)
+    value=$(echo $i | cut -d '=' -f 2-)
+    echo $key=$value
+    
+    # sed JS and CSS only
+    find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
+done
