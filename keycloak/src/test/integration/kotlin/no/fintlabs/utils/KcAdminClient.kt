532da4c614b2b62b80d1884eaa281970f2bf30e2
sivertheisholt <sivert.b.heisholt@protonmail.com>
2025-11-11 09:14:45 +0100

    feat: add tests for linking org/idp for flais-first-broker-login

--- keycloak/src/test/integration/kotlin/no/fintlabs/utils/KcAdminClient.kt
+++ keycloak/src/test/integration/kotlin/no/fintlabs/utils/KcAdminClient.kt
@@ -1,9 +1,11 @@
 package no.fintlabs.utils
 
+import org.keycloak.admin.client.CreatedResponseUtil
 import org.keycloak.admin.client.Keycloak
 import org.keycloak.admin.client.KeycloakBuilder
 import org.keycloak.admin.client.resource.RealmResource
 import org.keycloak.representations.idm.FederatedIdentityRepresentation
+import org.keycloak.representations.idm.MemberRepresentation
 import org.keycloak.representations.idm.UserRepresentation
 
 /**
@@ -33,6 +35,47 @@ object KcAdminClient {
         return kc to kc.realm(realm)
     }
 
+    fun addUserToOrg(
+        realm: RealmResource,
+        userId: String,
+        orgId: String,
+    ) {
+        realm
+            .organizations()
+            .get(orgId)
+            .members()
+            .addMember(userId)
+    }
+
+    fun createUser(
+        realm: RealmResource,
+        username: String,
+        email: String,
+        firstName: String,
+        lastName: String,
+        enabled: Boolean = true,
+    ): String {
+        val rep =
+            UserRepresentation().apply {
+                this.username = username
+                this.email = email
+                this.firstName = firstName
+                this.lastName = lastName
+                this.isEnabled = enabled
+            }
+
+        val resp = realm.users().create(rep)
+        val userId =
+            resp.use {
+                if (it.status !in 200..299) {
+                    throw IllegalStateException("Failed to create user: HTTP ${it.status}")
+                }
+                CreatedResponseUtil.getCreatedId(it)
+            }
+
+        return userId
+    }
+
     fun findUserByEmail(
         realm: RealmResource,
         email: String,
@@ -59,4 +102,18 @@ object KcAdminClient {
             .get(userId)
             .federatedIdentity
             .toList()
+
+    fun getOrgMember(
+        realm: RealmResource,
+        orgId: String,
+        userId: String,
+    ): MemberRepresentation? =
+        runCatching {
+            realm
+                .organizations()
+                .get(orgId)
+                .members()
+                .member(userId)
+                .toRepresentation()
+        }.getOrNull()
 }
