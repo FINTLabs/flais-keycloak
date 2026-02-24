4dc7dd3c01128ae1dfadb5ab5603e170f6adf9e2
jennifer-fintlabs <jennifer@fintlabs.no>
2026-02-24 13:29:32 +0100

    Update css to match novari design manual.

--- keycloak/libs/flais-theme/src/login/pages/components/OrgSelect.tsx
+++ keycloak/libs/flais-theme/src/login/pages/components/OrgSelect.tsx
@@ -1,44 +1,45 @@
-import React, { useMemo, useState } from 'react'
-import { I18n } from '../../i18n.ts'
-import { ImageDropdownInput } from './ImageDropdownInput.tsx'
+import React, { useMemo, useState } from "react";
+import { I18n } from "../../i18n.ts";
+import { ImageDropdownInput } from "./ImageDropdownInput.tsx";
 
 export interface OrgSelectProps {
-  i18n: I18n
-  organizations: {
-    alias: string
-    name: string
-    logo?: string
-  }[]
+    i18n: I18n;
+    organizations: {
+        alias: string;
+        name: string;
+        logo?: string;
+    }[];
 }
 
 const OrgSelectComponent = ({ i18n, organizations }: OrgSelectProps) => {
-  const [selectedIdp, setSelectedIdp] = useState<string>('')
+    const [selectedIdp, setSelectedIdp] = useState<string>("");
 
-  const options = useMemo(
-    () =>
-      organizations.map((org) => ({
-        id: org.alias,
-        label: org.name,
-        imageUrl: org.logo,
-      })),
-    [organizations]
-  )
+    const options = useMemo(
+        () =>
+            organizations.map((org) => ({
+                id: org.alias,
+                label: org.name,
+                imageUrl: org.logo,
+            })),
+        [organizations],
+    );
 
-  return (
-    <div>
-      <label htmlFor="org" className="sr-only">
-        {i18n.msgStr('chooseOrg')}
-      </label>
+    return (
+        <div>
+            <label htmlFor="selected_org" className="sr-only">
+                {i18n.msgStr("chooseOrg")}
+            </label>
 
-      <ImageDropdownInput
-        placeholder={i18n.msgStr('chooseOrg')}
-        name="selected_org"
-        options={options}
-        onChange={setSelectedIdp}
-        value={selectedIdp}
-      />
-    </div>
-  )
-}
+            <ImageDropdownInput
+                id="selected_org"
+                placeholder={i18n.msgStr("chooseOrg")}
+                name="selected_org"
+                options={options}
+                onChange={setSelectedIdp}
+                value={selectedIdp}
+            />
+        </div>
+    );
+};
 
-export const OrgSelect = React.memo(OrgSelectComponent)
+export const OrgSelect = React.memo(OrgSelectComponent);
