2b8a4439625f2941e99b231048c20cbd73a2b878
sivertheisholt <sivert.b.heisholt@protonmail.com>
2026-06-16 10:01:24 +0200

    chore: format code

--- keycloak/libs/flais-theme/src/login/pages/components/OrgSelect.tsx
+++ keycloak/libs/flais-theme/src/login/pages/components/OrgSelect.tsx
@@ -14,7 +14,7 @@ export interface OrgSelectProps {
 const getLogosUrl = (logo?: string) => {
   if (!logo) return undefined;
 
-  if(URL.canParse(logo)) return logo
+  if (URL.canParse(logo)) return logo;
 
   const baseUrl = import.meta.env.BASE_URL.replace(/\/$/, "");
   const filename = logo.replace(/^\/+/, "");
