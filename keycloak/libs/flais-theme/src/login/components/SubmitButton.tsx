49da67a8544dce1af0c7efcba00f768caa69da73
Sivert Heisholt <sivert.b.heisholt@protonmail.com>
2026-06-30 12:20:35 +0200

    FLA-1782: Use pointer cursor on hover for the continue action button (#372)

--- keycloak/libs/flais-theme/src/login/components/SubmitButton.tsx
+++ keycloak/libs/flais-theme/src/login/components/SubmitButton.tsx
@@ -11,7 +11,7 @@ const SubmitButtonComponent = ({ text }: SubmitButtonProps) => {
       className="
         w-full rounded-lg bg-primary px-4 py-3 text-sm font-medium uppercase text-white
         sm:py-3.5 sm:text-base
-        hover:bg-primary/90
+        hover:bg-primary/90 hover:cursor-pointer
         focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2
       "
     >
